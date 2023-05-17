60bc1591b5662fb5b905b434d88f4fd749357901
Noemi-Herbrik <xherbrik@stuba.sk>
2023-05-17 23:44:52 +0200

    JFET sk version

--- JFET/jfet_menu.js
+++ JFET/jfet_menu.js
@@ -5,6 +5,8 @@ menuList = [
 
 imgList = [
   ["NF_text", "NF_vztahy", "NF_def", "NF_priklad", "NF_orig_sch", "NF_nachr_sch"],
+  ["NF_text", "Sledovac_vztahy", "Sledovac_def", "Sledovac_priklad", "Sledovac_orig_sch", "Sledovac_nachr_sch"],
+  ["Dif_text", "Dif_vztahy", "Dif_def",  "Dif_orig_sch","Dif_orig_sch"],
   ["Zadanie"]
 ]
 
@@ -16,6 +18,9 @@ function resetMenuButtons() {
     document.getElementById(element[1]).innerHTML = "⮞"
 
   });
+  show_original_sch = true;
+  document.getElementById("sch_button_0").classList.remove("selected");
+  document.getElementById("sch_button_1").classList.remove("selected");
 }
 
 
@@ -82,15 +87,15 @@ function changeSchema(category) {
   img1 = document.getElementById(imgList[category][lastId-1])
   img2 = document.getElementById(imgList[category][lastId])
 
-
+  id = "sch_button_"+category
   if (show_original_sch){
-    document.getElementById("sch_button").classList.remove("selected");
+    document.getElementById(id).classList.remove("selected");
     img1.removeAttribute('hidden');
     img2.setAttribute('hidden', 'hidden');
   }
     
   else {
-     document.getElementById("sch_button").classList.add("selected");
+     document.getElementById(id).classList.add("selected");
      img2.removeAttribute('hidden');
      img1.setAttribute('hidden', 'hidden');
   }
